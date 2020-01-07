298910db93911388996d445dd8668653f4fed3fc
NihilistBrew <emanuel.johnsson@hotmail.com>
2020-01-07 16:41:32 +0100

    update

--- tools/make_readme.bat
+++ tools/make_readme.bat
@@ -5,9 +5,9 @@ for %%I in (.) do set base=%%~nxI
 if not "!base!"=="tools" (cd tools)
 
 cd ..
-copy docs\source\markdowns\*.md docs\source\markdowns\temp.md
-pandoc docs\source\markdowns\temp.md -o README.md
-del docs\source\markdowns\temp.md
+copy docs\source\*.md docs\source\temp.md
+pandoc docs\source\temp.md -o README.md
+del docs\source\temp.md
 
 copy /A README.md README_temp.md /B > nul
 del README.md
